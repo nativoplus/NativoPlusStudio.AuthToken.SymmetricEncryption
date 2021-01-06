7116a2ced31c6263b9b0f6ff9b7e09540bcb4c39
Angel Ramos <aramos@techcu.com>
2021-01-05 17:29:40 -0800

    update core

--- NativoPlusStudio.AuthToken.SymmetricEncryption/Extensions/AuthTokenSymmetricEncryptionServices.cs
+++ NativoPlusStudio.AuthToken.SymmetricEncryption/Extensions/AuthTokenSymmetricEncryptionServices.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using NativoPlusStudio.AuthToken.Core;
+using NativoPlusStudio.Encryption.Interfaces;
 using NativoPlusStudio.Encryption.Services;
 using Serilog;
 
@@ -7,6 +8,10 @@ namespace NativoPlusStudio.AuthToken.SymmetricEncryption.Extensions
 {
     public static class AuthTokenSymmetricEncryptionServices
     {
+        public static void AddSymmetricEncryption<TEncryptionImplementation>(this AuthTokenServicesBuilder builder, TEncryptionImplementation encryptionImplementation) where TEncryptionImplementation : IEncryption, new()
+        {
+            builder.AddAuthTokenEncryptionImplementation(encryptionImplementation);
+        }
         public static void AddSymmetricEncryption(this AuthTokenServicesBuilder builder, string primaryPrivateKey, string secondaryPrivateKey = null)
         {
             var serviceProvider = builder.Services.BuildServiceProvider();
